1a2295ef84a013371d63fa8fc46b9659207fe16a
makey72 <makeev.gleb@yandex.ru>
2023-06-17 16:05:07 +0300

    update

--- src/js/components/ex.js
+++ src/js/components/ex.js
@@ -1,1 +1,34 @@
-console.log('maxgraph');
+import {validateForms} from "../functions/validate-forms";
+
+const setting = [
+  {
+    ruleSelector: '#inp-name', rules: [
+      {
+        rule: 'required',
+      },
+      {
+        rule: 'minLength',
+        value: 3,
+      },
+    ]
+  },
+  {
+    ruleSelector: '#inp-number',
+    tel: true,
+    telError: 'err phone',
+    rules: [
+      {
+        rule: 'required',
+      },
+      {
+        rule: 'minLength',
+        value: 10,
+      },
+    ],
+  }
+]
+
+const send = () => {
+  console.log(1)
+}
+validateForms('.form', setting, send())
