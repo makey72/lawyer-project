aaa1e803853587fcda7115c36715c89c6c937b83
makey72 <makeev.gleb@yandex.ru>
2023-07-08 21:44:45 +0300

    update

--- src/js/functions/validate-forms.js
+++ src/js/functions/validate-forms.js
@@ -6,7 +6,6 @@ export const validateForms = (selector, rules, afterSend) => {
   const telSelector = form?.querySelector('input[type="tel"]');
 
   if (!form) {
-    console.error('Нет такого селектора!');
     return false;
   }
 
